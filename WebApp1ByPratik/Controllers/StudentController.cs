b02d2acc95a8e0c049b834ad05dfdf459a80357f
Pratik Shah <111047921+Lucifer98712@users.noreply.github.com>
2024-09-15 10:54:38 +0545

    Create WebApp1

---
+++ WebApp1ByPratik/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp1ByPratik.Models;
+
+namespace WebApp1ByPratik.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyRazorPage()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+            
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student s) 
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Details", s);
+            }
+            else
+            {
+                return View(s);
+            }
+
+        }
+
+        
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp1ByPratik/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp1ByPratik.Models
+{
+    public class Student
+    {
+        [Range(1, 1000, ErrorMessage = "ID must be between 1 and 1000")]
+        public int StdID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Faculty is required")]
+
+        public string Faculty { get; set; }
+    }
+}
