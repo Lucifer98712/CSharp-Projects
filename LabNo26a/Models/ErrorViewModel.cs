457682f0c00cb71d9e7f82ce12a379207f53782f
Pratik Shah <111047921+Lucifer98712@users.noreply.github.com>
2024-09-16 12:59:27 +0545

    Create Lab

---
+++ LabNo26a/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LabNo26a.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
