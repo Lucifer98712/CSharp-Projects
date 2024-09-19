7b1243cbdebf861e8664fe8dc6e715264a5d5478
Pratik Shah <111047921+Lucifer98712@users.noreply.github.com>
2024-09-19 14:06:02 +0545

    Create WebApp4

---
+++ WebApp4ByPratik/Controllers/ProductController.cs
@@ -0,0 +1,124 @@
+﻿using EFCoreEg.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using EFCoreEg.Models;
+using Microsoft.Data.SqlClient;
+
+namespace EFCoreEg.Controllers
+{
+    public class ProductController : Controller
+    {
+       IRepository<Product> _repo = null;
+
+        public ProductController(IRepository<Product> repo)
+        {
+            _repo = repo;
+        }
+
+        //GET: ProductController
+
+        public ActionResult Index()
+        {
+            return View(_repo.GetAllRecords());
+        }
+
+        // GET: ProductController/Details/S
+
+        public ActionResult Details(int id) 
+        {
+            return View(_repo.GetSingleRecord(id));
+        }
+
+        // GET: ProductController/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        public ActionResult Create(Product prod) 
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repo.AddRecord(prod);
+                    return Content("Record has been inserted !");
+                }
+                else
+                {
+                    return View(prod);
+                }
+            }
+            catch (SqlException ex) 
+            {
+                return Content("Something wrong" + ex.Message);
+            }
+        }
+
+        // GET: ProductController/Edit/S
+
+        public ActionResult Edit(int id) 
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/S
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product prod)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repo.UpdateRecord(prod);
+                    return Content("Record has been edited !");
+                }
+                else
+                {
+                    return View(prod);
+                }
+            }
+            catch (SqlException ex)
+            {
+                return Content("Something wrong " + ex.Message);
+            }
+
+        }
+
+        // GET: ProductController/Delete/S
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/S
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Product prod)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repo.DeleteRecord(prod);
+                    return Content("Record has been deleted !");
+                }
+                else
+                {
+                    return View(prod);
+                }
+            }
+            catch (SqlException ex)
+            {
+                return Content("Something wrong " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ WebApp4ByPratik/Repositories/ProductRepo.cs
@@ -0,0 +1,47 @@
+﻿using EFCoreEg.Models;
+
+namespace EFCoreEg.Repositories
+{
+    public class ProductRepo : IRepository<Product>
+    {
+        ProductDbContext _context = null;
+
+        public ProductRepo(ProductDbContext context)
+        {
+            _context = context;
+        }
+        public void AddRecord(Product model)
+        {
+            _context.Add(model);
+            _context.SaveChanges();
+        }
+
+        public Product DeleteRecord(Product model)
+        {
+            _context.Remove(model);
+            return model;
+        }
+
+        public IEnumerable<Product> GetAllRecords()
+        {
+
+            List<Product> allProducts = _context.Products.ToList();
+            return allProducts;
+        }    
+            
+         
+
+        public Product GetSingleRecord(int id)
+        {
+            Product p = _context.Products.Find(id);
+            return p;
+        }
+
+        public Product UpdateRecord(Product model)
+        {
+            _context.Update(model);
+            _context.SaveChanges();
+            return model;
+        }
+    }
+}
